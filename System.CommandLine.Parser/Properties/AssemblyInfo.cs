7d6b3da1fe7686cf2f87c1a3d30f3dbd52d2911c
David Neumann <david.neumann@live.com>
2016-03-02 11:41:58 +0100

    Created the new solution for the command line parser.

---
+++ System.CommandLine.Parser/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+﻿
+#region Using Directives
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+
+#region General Assembly Information
+
+[assembly: AssemblyTitle("Command Line Parser")]
+[assembly: AssemblyDescription("A .NET library, which can be used to parse command line parameters and make them available in strongly typed objects.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Command Line Parser")]
+[assembly: AssemblyCopyright("Copyright © David Neumann 2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5e4fb20c-183e-46e0-ac8a-53d6637184a5")]
+
+#endregion
+
+#region Assembly Version Information
+
+[assembly: AssemblyVersion("0.1.0.*")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+
+#endregion
