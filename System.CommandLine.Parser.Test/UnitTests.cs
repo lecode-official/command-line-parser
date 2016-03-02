e3e02d4d9b3b83494eaa99edd68e3d78d0963127
David Neumann <david.neumann@live.com>
2016-03-02 11:49:41 +0100

    Created a unit testing project for the command line parser.

---
+++ System.CommandLine.Parser.Test/UnitTests.cs
@@ -0,0 +1,28 @@
+﻿
+#region Using Directives
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#endregion
+
+namespace System.CommandLine.Parser.Test
+{
+    /// <summary>
+    /// Represents a unit testing class, which contains all of the unit tests for the command line parser.
+    /// </summary>
+    [TestClass]
+    public class UnitTests
+    {
+        #region Test Methods
+
+        /// <summary>
+        /// A first test method.
+        /// </summary>
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        #endregion
+    }
+}
