c4c8486f0e9fc5b8abf8fb0423150d1ae5381f46
David Neumann <david.neumann@lecode.de>
2016-03-22 16:53:37 +0100

    Started to work on the actual command line parser.

---
+++ System.CommandLine.Parser/CommandLineParameter.cs
@@ -0,0 +1,23 @@
+﻿
+namespace System.CommandLine.Parser
+{
+    /// <summary>
+    /// Represents the abstract base class for the typed command line parameters.
+    /// </summary>
+    public abstract class CommandLineParameter<T>
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the name of the command line parameter.
+        /// </summary>
+        public string Name { get; internal set; }
+
+        /// <summary>
+        /// Gets the value of the command line parameter.
+        /// </summary>
+        public T Value { get; internal set; }
+
+        #endregion
+    }
+}
---
+++ System.CommandLine.Parser/CommandLineParser.cs
@@ -0,0 +1,29 @@
+﻿
+namespace System.CommandLine.Parser
+{
+    /// <summary>
+    /// Represents a parser, which is able to parse command line parameters and convert them to strongly-typed .NET data types.
+    /// </summary>
+    public static class CommandLineParser
+    {
+        #region Public Static Methods
+
+        /// <summary>
+        /// Retrieves the command line parameters that have been passed to the program via the command line.
+        /// </summary>
+        /// <returns>Returns the command line parameters that have been passed to the program via the command line.</returns>
+        public static string RetrieveCommandLineParameters()
+        {
+            // Gets the command line parameters that have been passed to the program
+            string commandLineParameters = Environment.CommandLine;
+
+            // The command line parameters start with image file name, from which the program was loaded, this is unuseful during parsing and is therefore removed
+
+
+            // Returns the retrieved command line parameters
+            return commandLineParameters;
+        }
+
+        #endregion
+    }
+}
