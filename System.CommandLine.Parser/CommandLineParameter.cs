f763940674a370ddf539cd74494c7e62be92e329
David Neumann <david.neumann@lecode.de>
2016-03-22 16:58:16 +0100

    Fixed some problems, which caused the build to fail.

--- System.CommandLine.Parser/CommandLineParameter.cs
+++ System.CommandLine.Parser/CommandLineParameter.cs
@@ -4,7 +4,7 @@ namespace System.CommandLine.Parser
     /// <summary>
     /// Represents the abstract base class for the typed command line parameters.
     /// </summary>
-    public abstract class CommandLineParameter<T>
+    public abstract class CommandLineParameter
     {
         #region Public Properties
 
@@ -12,12 +12,7 @@ namespace System.CommandLine.Parser
         /// Gets the name of the command line parameter.
         /// </summary>
         public string Name { get; internal set; }
-
-        /// <summary>
-        /// Gets the value of the command line parameter.
-        /// </summary>
-        public T Value { get; internal set; }
-
+        
         #endregion
     }
 }
