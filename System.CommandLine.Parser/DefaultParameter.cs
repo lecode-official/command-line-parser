d99e00759bf521e4b00c71c30f94a5cd1ac9824a
David Neumann <david.neumann@live.com>
2016-03-22 21:52:56 +0100

    Joined together the default parameters and the named parameters.

---
+++ System.CommandLine.Parser/DefaultParameter.cs
@@ -0,0 +1,49 @@
+﻿
+namespace System.CommandLine.Parser
+{
+    /// <summary>
+    /// Represents a default parameter.
+    /// </summary>
+    public class DefaultParameter : Parameter
+    {
+        #region Parameter Implementation
+
+        /// <summary>
+        /// Gets the name of the parameter, always returns "Default".
+        /// </summary>
+        public override string Name
+        {
+            get
+            {
+                return "Default";
+            }
+
+            internal set
+            {
+                throw new InvalidOperationException("The name of a default parameter can not be changed.");
+            }
+        }
+
+        /// <summary>
+        /// Gets a value that determines whether this parameter is a default parameter. Always returns <c>true</c>.
+        /// </summary>
+        public override bool IsDefaultParameter
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the value of the default parameter.
+        /// </summary>
+        public string Value { get; internal set; }
+
+        #endregion
+    }
+}
