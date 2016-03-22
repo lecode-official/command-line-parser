c95b1c40d92964f61045149fc7418772f93718d5
David Neumann <david.neumann@live.com>
2016-03-22 16:53:37 +0100

    Started to work on the actual command line parser.

---
+++ System.CommandLine.Parser/ParameterBag.cs
@@ -0,0 +1,29 @@
+﻿
+#region Using Directives
+
+using System.Collections.Generic;
+
+#endregion
+
+namespace System.CommandLine.Parser
+{
+    /// <summary>
+    /// Represents a bag, which is able to contain parsed properties.
+    /// </summary>
+    public class ParameterBag
+    {
+        #region Public Properties
+
+        /// <summary>
+        /// Gets the original command line parameters from which the properties were parsed.
+        /// </summary>
+        public string CommandLineParameters { get; internal set; }
+
+        /// <summary>
+        /// Gets a collection of the command line parameters that have been parsed.
+        /// </summary>
+        public IEnumerable<CommandLineParameter> Parameters { get; internal set; } = new List<CommandLineParameter>();
+
+        #endregion
+    }
+}
