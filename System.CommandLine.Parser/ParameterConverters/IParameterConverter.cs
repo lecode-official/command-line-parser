fb2437d23899443bb0075d955678ce5ce1b2517c
David Neumann <david.neumann@lecode.de>
2016-04-09 18:11:13 +0200

    The currently used algorithm for converting parameters into real POCO values is static, horribly complicated, and never really worked well. Therefore a new algorithm is introduced, which uses parameter converters, which work much better, are more flexible, and easier to understand. This commit contains the first step: the base interface for the parameter converters.

---
+++ System.CommandLine.Parser/ParameterConverters/IParameterConverter.cs
@@ -0,0 +1,36 @@
+﻿
+#region Using Directives
+
+using System.CommandLine.Parser.Parameters;
+
+#endregion
+
+namespace System.CommandLine.Parser.ValueConverters
+{
+    /// <summary>
+    /// Represents the interface that all parameter converters have to implement. Parameter converters are used to convert the parsed parameters into the real values that are
+    /// injected into the POCO objects.
+    /// </summary>
+    public interface IParameterConverter
+    {
+        #region Methods
+
+        /// <summary>
+        /// Dertermines whether the specified parameter can be converted into the specified type.
+        /// </summary>
+        /// <param name="propertyType">The type of the property into which the parameter is to be converted.</param>
+        /// <param name="parameter">The parameters that is to be converted into the specified type.</param>
+        /// <returns>Returns <c>true</c> if the specified parameter can be converted into the specified type and <c>false</c> otherwise.</returns>
+        bool CanConvert(Type propertyType, Parameter parameter);
+
+        /// <summary>
+        /// Converts the specified parameter into the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type into which the parameter is to be converted.</typeparam>
+        /// <param name="parameter">The parameter that is to be converted into the specified type.</param>
+        /// <returns>Returns the converted parameter value.</returns>
+        T Convert<T>(Parameter parameter);
+
+        #endregion
+    }
+}
