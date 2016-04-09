6bb75cacebe7c384519f13d25530386f44c0d627
David Neumann <david.neumann@live.com>
2016-04-09 17:56:16 +0200

    Moved the parameter classes into their own namespace.

--- System.CommandLine.Parser/Parameters/ArrayParameter.cs
+++ System.CommandLine.Parser/Parameters/ArrayParameter.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 
 #endregion
 
-namespace System.CommandLine.Parser
+namespace System.CommandLine.Parser.Parameters
 {
     /// <summary>
     /// Represents an array command line parameter.
