f65607f39e3c8844392b9c5ec106b701016e5e78
David Neumann <david.neumann@lecode.de>
2016-06-28 11:19:00 +0200

    Joined the parser errors in a single class.

--- System.CommandLine.Parser/ParserError.cs
+++ System.CommandLine.Parser/ParserError.cs
@@ -2,21 +2,23 @@
 namespace System.CommandLine.Parser
 {
     /// <summary>
-    /// Represents an error, which occurred while lexing the input character stream.
+    /// Represents an error, which occurred while lexing or parsing the input.
     /// </summary>
-    public class LexicalError
+    public class ParserError
     {
         #region Constructors
 
         /// <summary>
-        /// Initializes a new <see cref="LexicalError"/> instance.
+        /// Initializes a new <see cref="ParserError"/> instance.
         /// </summary>
         /// <param name="startIndex">The index at which the offending symbol starts.</param>
-        /// <param name="offendingSymbol">The symbol that caused the lexical error.</param>
-        public LexicalError(int startIndex, string offendingSymbol)
+        /// <param name="offendingSymbol">The symbol that caused the error.</param>
+        /// <param name="kind">The kind of error that happened.</param>
+        public ParserError(int startIndex, string offendingSymbol, ParserErrorKind kind)
         {
             this.StartIndex = startIndex;
             this.OffendingSymbol = offendingSymbol;
+            this.Kind = kind;
         }
 
         #endregion
@@ -29,9 +31,14 @@ namespace System.CommandLine.Parser
         public int StartIndex { get; private set; }
 
         /// <summary>
-        /// Gets the symbol that caused the lexical error.
+        /// Gets the symbol that caused the error.
         /// </summary>
         public string OffendingSymbol { get; private set; }
+
+        /// <summary>
+        /// Gets the kind of error that happened.
+        /// </summary>
+        public ParserErrorKind Kind { get; private set; }
 
         #endregion
     }
