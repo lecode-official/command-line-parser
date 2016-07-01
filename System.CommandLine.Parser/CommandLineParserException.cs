beddea0107e074a1dba3c4767d1ac62206d7c564
David Neumann <david.neumann@lecode.de>
2016-07-01 14:18:08 +0200

    Now a specialized exception is thrown when a lexical or syntactical error occurs during the parsing of the command line parameters.

---
+++ System.CommandLine.Parser/CommandLineParserException.cs
@@ -0,0 +1,63 @@
+﻿
+#region Using Directives
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+
+#endregion
+
+namespace System.CommandLine.Parser
+{
+    public class CommandLineParserException : Exception
+    {
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new <see cref="CommandLineParserException"/> instance.
+        /// </summary>
+        /// <param name="parserErrors">The parser error that occurred.</param>
+        public CommandLineParserException(IEnumerable<ParserError> parserErrors)
+            : this($"{parserErrors.Where(error => error.Kind == ParserErrorKind.Lexical).Count()} lexical error(s) and {parserErrors.Where(error => error.Kind == ParserErrorKind.Syntactical).Count()} syntactical error(s) occurred during the parsing of the command line parameters.")
+        {
+            this.ParserErrors = parserErrors;
+        }
+
+        /// <summary>
+        /// Initializes a new <see cref="CommandLineParserException"/> instance.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        public CommandLineParserException(string message)
+            : base(message)
+        { }
+
+        /// <summary>
+        /// Initializes a new <see cref="CommandLineParserException"/> instance.
+        /// </summary>
+        /// <param name="message">The error message that explains the reason for the exception.</param>
+        /// <param name="innerException">The exception that is the cause of the current exception, or a <c>null</c> reference if no inner exception is specified.</param>
+        public CommandLineParserException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+
+        /// <summary>
+        /// Initializes a new <see cref="CommandLineParserException"/> instance.
+        /// </summary>
+        /// <param name="info">The <see cref="SerializationInfo"/> that holds the serialized object data about the exception being thrown.</param>
+        /// <param name="context">The <see cref="StreamingContext"/> that contains contextual information about the source or destination.</param>
+        protected CommandLineParserException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        { }
+
+        #endregion
+
+        #region Public Properties
+
+        /// <summary>
+        /// Gets all the parser error that occurred.
+        /// </summary>
+        public IEnumerable<ParserError> ParserErrors { get; private set; } = new List<ParserError>();
+
+        #endregion
+    }
+}
