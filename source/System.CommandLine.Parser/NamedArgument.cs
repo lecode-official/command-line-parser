5bd1a885116b9bdf3d534be62501ec9e9131ebc9
David Neumann <david.neumann@lecode.de>
2017-09-13 12:43:25 +0200

    Fixed some minor problems with the code.

--- source/System.CommandLine.Parser/NamedArgument.cs
+++ source/System.CommandLine.Parser/NamedArgument.cs
@@ -8,10 +8,10 @@ using System.Reflection;
 namespace System.CommandLine
 {
     /// <summary>
-    /// Represents a single argument of a command line parser.
+    /// Represents a single named argument of a command line parser. A named argument is a command line argument that is comprised of a name and an explicit value.
     /// </summary>
     /// <typeparam name="T">The type of the argument.</param>
-    public class NamedArgument<T> : IArgument
+    public class NamedArgument<T> : Argument
     {
         #region Constructors
 
@@ -24,7 +24,9 @@ namespace System.CommandLine
         /// <param name="help">A descriptive help text for the argument, which is used in the help string.</param>
         /// <param name="defaultValue">The value that the argument receives if it was not detected by the parser.</param>
         /// <param name="duplicateResolutionPolicy">A callback function, which is invoked when the same argument was specified twice.</param>
-        /// <exception cref="ArgumentNullException">If either the name, the alias, the destination, the default value, or the duplicate resolution policy are <c>null</c>, then an <see cref="ArgumentNullException"/> is thrown.</exception>
+        /// <exception cref="ArgumentNullException">
+        /// If either the name, the alias, the destination, the default value, or the duplicate resolution policy are <c>null</c>, then an <see cref="ArgumentNullException"/> is thrown.
+        /// </exception>
         public NamedArgument(string name, string alias, string destination, string help, T defaultValue, Func<T, T, T> duplicateResolutionPolicy)
         {
             // Validates the arguments
@@ -46,45 +48,12 @@ namespace System.CommandLine
             this.Help = help;
             this.DefaultValue = defaultValue;
             this.DuplicateResolutionPolicy = duplicateResolutionPolicy;
+            this.Type = typeof(T);
         }
 
         #endregion
 
         #region Public Properties
-
-        /// <summary>
-        /// Gets the name of the argument, which is used for parsing and in the help string.
-        /// </summary>
-        public string Name { get; private set; }
-
-        /// <summary>
-        /// Gets the alias name of the argument.
-        /// </summary>
-        public string Alias { get; private set; }
-
-        /// <summary>
-        /// Contains the name that the argument will have in the result dictionary after parsing. This should adhere to normal C# naming standards.
-        /// </summary>
-        private string destination;
-
-        /// <summary>
-        /// Gets the name that the argument will have in the result dictionary after parsing. This should adhere to normal C# naming standards.
-        /// </summary>
-        public string Destination
-        {
-            get => this.destination;
-            set => this.destination = value.ToCamelCasePropertyName();
-        }
-
-        /// <summary>
-        /// Gets the descriptive help text for the argument, which is used in the help string.
-        /// </summary>
-        public string Help { get; private set; }
-
-        /// <summary>
-        /// Gets the type of the argument.
-        /// </summary>
-        public Type Type { get => typeof(T); }
 
         /// <summary>
         /// Gets the value that the argument receives if it was not detected by the parser.
