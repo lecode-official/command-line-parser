21f319299ea5d4da3aba193ee87f8520a263d057
David Neumann <david.neumann@lecode.de>
2017-09-21 09:20:43 +0200

    Moved the collection helper into an appropriate namespace.

--- source/System.CommandLine.Parser/ValueConverters/CollectionHelper.cs
+++ source/System.CommandLine.Parser/ValueConverters/CollectionHelper.cs
@@ -9,7 +9,7 @@ using System.Reflection;
 
 #endregion
 
-namespace System.CommandLine.ValueConverts
+namespace System.CommandLine.ValueConverters
 {
     /// <summary>
     /// Represents a helper, which makes it easy to create collections of many different kinds via one, simple API.
