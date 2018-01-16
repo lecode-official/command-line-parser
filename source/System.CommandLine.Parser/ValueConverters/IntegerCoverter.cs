484c98b0f0db570f58ddfd2c62f1ba8f7aee0c1f
David Neumann <david.neumann@lecode.de>
2018-01-16 10:50:23 +0100

    Updated the documentation comments of the value converters to contain all the exception that may be thrown.

--- source/System.CommandLine.Parser/ValueConverters/IntegerCoverter.cs
+++ source/System.CommandLine.Parser/ValueConverters/IntegerCoverter.cs
@@ -34,12 +34,7 @@ namespace System.CommandLine.ValueConverters
         /// </summary>
         /// <param name="type">The type which is to be tested.</param>
         /// <returns>Returns <c>true</c> if the value converter can convert to the specified type and <c>false</c> otherwise.</returns>
-        public bool CanConvertFrom(string value) => long.TryParse(
-            value,
-            numberStyles,
-            CultureInfo.InvariantCulture,
-            out _
-        );
+        public bool CanConvertFrom(string value) => long.TryParse(value, numberStyles, CultureInfo.InvariantCulture, out _);
 
         /// <summary>
         /// Determines whether this value converter is able to convert the specified string.
@@ -54,6 +49,7 @@ namespace System.CommandLine.ValueConverters
         /// <param name="value">The value that is to be converted to the specified destination type.</param>
         /// <param name="type">The type to which the value is to be converted.</param>
         /// <exception cref="ArgumentNullException">If the value is <c>null</c>, empty, or only consists of white spaces, then an <see cref="ArgumentNullException"/> is thrown.
+        /// <exception cref="InvalidOperationException">If the specified type is not supported or the value could not be converted, then an <see cref="InvalidOperationException"/> is thrown.
         /// <returns>Returns a new instance of the specified type, that contains the converted value.</returns>
         public object Convert(Type type, string value)
         {
