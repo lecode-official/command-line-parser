ef110c331811d4f29972b77f2b451ebe849f10c7
David Neumann <david.neumann@lecode.de>
2018-01-17 14:31:58 +0100

    Found a bug in the camel casing extension method and fixed it.

--- test/System.CommandLine.Parser.Tests/CamelCasingTests.cs
+++ test/System.CommandLine.Parser.Tests/CamelCasingTests.cs
@@ -17,14 +17,16 @@ namespace System.CommandLine.Parser.Tests
         #region Unit Tests
 
         /// <summary>
-        /// Tests the camel-casing methods used for the positional arguments.
+        /// Tests the camel-casing methods used for the named arguments.
         /// </summary>
         [Fact]
         public void TestCamelCasing()
         {
-            Assert.Equal("ThisIsATest", "tHiS iS a TeSt".ToCamelCasePropertyName());
+            Assert.Equal("ThisIsATest", "This is a Test".ToCamelCasePropertyName());
             Assert.Equal("ThisIsATest", "this_is_a_test".ToCamelCasePropertyName());
+            Assert.Equal("ThisIsATest", "ThisIsATest".ToCamelCasePropertyName());
             Assert.Equal("ThisIsATest", "--this-is-a-test".ToCamelCasePropertyName());
+            Assert.Equal("ThisIsATest", "/this_is_a_test".ToCamelCasePropertyName());
             Assert.Equal("ThisIsATest", " this is a test ".ToCamelCasePropertyName());
             Assert.Equal("ThisIsATest", "T!h§i%s_i&s_a_t/e(s)t".ToCamelCasePropertyName());
             Assert.Equal("_45ThisIsATest123", "45this is a test123".ToCamelCasePropertyName());
