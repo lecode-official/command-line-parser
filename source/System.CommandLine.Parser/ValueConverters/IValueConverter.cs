6e15841f0cbd312d886ee091a94ceaac9e029901
David Neumann <david.neumann@lecode.de>
2018-01-17 13:45:29 +0100

    Renamed some parameters to be more consistent with the rest of the API.

--- source/System.CommandLine.Parser/ValueConverters/IValueConverter.cs
+++ source/System.CommandLine.Parser/ValueConverters/IValueConverter.cs
@@ -18,19 +18,19 @@ namespace System.CommandLine.ValueConverters
         /// <summary>
         /// Determines whether this value converter is able to convert a string to the specified type.
         /// </summary>
-        /// <param name="type">The type which is to be tested.</param>
+        /// <param name="resultType">The type which is to be tested.</param>
         /// <returns>Returns <c>true</c> if the value converter can convert to the specified type and <c>false</c> otherwise.</returns>
-        bool CanConvertTo(Type type);
+        bool CanConvertTo(Type resultType);
 
         /// <summary>
         /// Converts the specified value to the specified destination type.
         /// </summary>
         /// <param name="value">The value that is to be converted to the specified destination type.</param>
-        /// <param name="type">The type to which the value is to be converted.</param>
+        /// <param name="resultType">The type to which the value is to be converted.</param>
         /// <exception cref="ArgumentNullException">If the type or the value is <c>null</c>, empty, or only consists of white spaces, then an <see cref="ArgumentNullException"/> is thrown.
         /// <exception cref="InvalidOperationException">If the specified type is not supported or the value could not be converted, then an <see cref="InvalidOperationException"/> is thrown.
         /// <returns>Returns a new instance of the specified type, that contains the converted value.</returns>
-        object Convert(Type type, string value);
+        object Convert(Type resultType, string value);
 
         #endregion
     }
