5ed6be981bd6ce064e88d9457537dbd7cd767db6
David Neumann <david.neumann@lecode.de>
2018-01-17 13:45:04 +0100

    Fixed some documentation comments (the documentation comments of two methods were flipped).

--- source/System.CommandLine.Parser/ValueConverters/FloatConverter.cs
+++ source/System.CommandLine.Parser/ValueConverters/FloatConverter.cs
@@ -30,49 +30,49 @@ namespace System.CommandLine.ValueConverters
         #region IValueConverter Implementation
 
         /// <summary>
-        /// Determines whether this value converter is able to convert a string to the specified type.
-        /// </summary>
-        /// <param name="type">The type which is to be tested.</param>
-        /// <returns>Returns <c>true</c> if the value converter can convert to the specified type and <c>false</c> otherwise.</returns>
-        public bool CanConvertFrom(string value) => decimal.TryParse(value, numberStyles, CultureInfo.InvariantCulture, out _);
-
-        /// <summary>
         /// Determines whether this value converter is able to convert the specified string.
         /// </summary>
         /// <param name="value">The value that is to be tested.</param>
         /// <returns>Returns <c>true</c> if the value converter is able to convert the specified type and <c>false</c> otherwise.</returns>
-        public bool CanConvertTo(Type type) => FloatConverter.supportedTypes.Contains(type);
+        public bool CanConvertFrom(string value) => decimal.TryParse(value, numberStyles, CultureInfo.InvariantCulture, out _);
+
+        /// <summary>
+        /// Determines whether this value converter is able to convert a string to the specified type.
+        /// </summary>
+        /// <param name="resultType">The type which is to be tested.</param>
+        /// <returns>Returns <c>true</c> if the value converter can convert to the specified type and <c>false</c> otherwise.</returns>
+        public bool CanConvertTo(Type resultType) => FloatConverter.supportedTypes.Contains(resultType);
 
         /// <summary>
         /// Converts the specified value to the specified destination type.
         /// </summary>
         /// <param name="value">The value that is to be converted to the specified destination type.</param>
-        /// <param name="type">The type to which the value is to be converted.</param>
+        /// <param name="resultType">The type to which the value is to be converted.</param>
         /// <exception cref="ArgumentNullException">If the type or the value is <c>null</c>, empty, or only consists of white spaces, then an <see cref="ArgumentNullException"/> is thrown.
         /// <exception cref="InvalidOperationException">If the specified type is not supported or the value could not be converted, then an <see cref="InvalidOperationException"/> is thrown.
         /// <returns>Returns a new instance of the specified type, that contains the converted value.</returns>
-        public object Convert(Type type, string value)
+        public object Convert(Type resultType, string value)
         {
             // Validates the arguments
-            if (type == null)
-                throw new ArgumentNullException(nameof(type));
+            if (resultType == null)
+                throw new ArgumentNullException(nameof(resultType));
             if (string.IsNullOrWhiteSpace(value))
                 throw new ArgumentNullException(nameof(value));
 
             // Tries to convert the specified value to the specified type
             try
             {
-                if (type == typeof(float))
+                if (resultType == typeof(float))
                     return float.Parse(value, numberStyles, CultureInfo.InvariantCulture);
-                if (type == typeof(double))
+                if (resultType == typeof(double))
                     return double.Parse(value, numberStyles, CultureInfo.InvariantCulture);
-                if (type == typeof(decimal))
+                if (resultType == typeof(decimal))
                     return decimal.Parse(value, numberStyles, CultureInfo.InvariantCulture);
             }
             catch (FormatException) {}
 
             // If this code is reached it means, that either the specified type is not supported or the value could not be converted, in that case an exception is thrown
-            throw new InvalidOperationException($"The value \"{value}\" cannot be converted to \"{type.Name}\".");
+            throw new InvalidOperationException($"The value \"{value}\" cannot be converted to \"{resultType.Name}\".");
         }
 
         #endregion
