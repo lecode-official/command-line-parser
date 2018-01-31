98acb13b781dcb00d507e8cef9a63354a16f1122
David Neumann <david.neumann@lecode.de>
2018-01-31 17:16:05 +0100

    Now integer values can also be converted to enumeration values.

--- test/ValueConverterTests.cs
+++ test/ValueConverterTests.cs
@@ -34,6 +34,32 @@ namespace System.CommandLine.Parser.Tests
             Assert.Equal(false, ValueConverter.Convert(typeof(bool), "0"));
         }
 
+        /// <summary>
+        /// Represents an enumeration used for testing the conversion of enumeration values.
+        /// </summary>
+        [Flags]
+        private enum Color { Red = 1, Green = 2, Blue = 4 }
+
+        /// <summary>
+        /// Tests how the <see ref="ValueConverter"/> handles the conversion of enumeration values.
+        /// </summary>
+        [Fact]
+        public void TestEnumerationValueConversion()
+        {
+            Assert.Equal(Color.Red, ValueConverter.Convert(typeof(Color), "Red"));
+            Assert.Equal(Color.Green, ValueConverter.Convert(typeof(Color), "Green"));
+            Assert.Equal(Color.Blue, ValueConverter.Convert(typeof(Color), "Blue"));
+            Assert.Equal(Color.Red | Color.Blue, ValueConverter.Convert(typeof(Color), "5"));
+
+            Assert.Equal(DayOfWeek.Monday, ValueConverter.Convert(typeof(DayOfWeek), "Monday"));
+            Assert.Equal(DayOfWeek.Tuesday, ValueConverter.Convert(typeof(DayOfWeek), "Tuesday"));
+            Assert.Equal(DayOfWeek.Wednesday, ValueConverter.Convert(typeof(DayOfWeek), "Wednesday"));
+            Assert.Equal(DayOfWeek.Thursday, ValueConverter.Convert(typeof(DayOfWeek), "Thursday"));
+            Assert.Equal(DayOfWeek.Friday, ValueConverter.Convert(typeof(DayOfWeek), "Friday"));
+            Assert.Equal(DayOfWeek.Saturday, ValueConverter.Convert(typeof(DayOfWeek), "Saturday"));
+            Assert.Equal(DayOfWeek.Sunday, ValueConverter.Convert(typeof(DayOfWeek), "Sunday"));
+        }
+
         #endregion
     }
 }
